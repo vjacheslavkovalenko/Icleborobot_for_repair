56e26af50cacd8fc979341f1010a61c5c451962f
vjacheslavkovalenko <vjacheslavkovalenko@gmail.com>
2024-01-21 00:45:57 +0300

    Commit N3. SwipeRefreshLayout. ProgressBar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
+    implementation ("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/icleborobot_for_repair/SiteFragment.kt
+++ app/src/main/java/com/example/icleborobot_for_repair/SiteFragment.kt
@@ -1,7 +1,10 @@
 package com.example.icleborobot_for_repair
 
 import android.annotation.SuppressLint
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.KeyEvent
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,11 +12,15 @@ import android.view.View
 import android.view.ViewGroup
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.example.icleborobot_for_repair.databinding.FragmentSiteBinding
 
 class SiteFragment : Fragment() {
 
 //    private lateinit var binding_site: FragmentSiteBinding
+    private lateinit var swipeRefreshLayout: SwipeRefreshLayout
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,11 +30,16 @@ class SiteFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_site, container, false)
     }
 
+
+
     @SuppressLint("SetJavaScriptEnabled")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?){
+        //lateinit var swipeRefreshLayout: SwipeRefreshLayout
+        //var swipeRefreshLayout: SwipeRefreshLayout = view.findViewById(R.id.swipe_fragment_site)
         val siteWebView: WebView = view.findViewById(R.id.site_web_view)
         siteWebView.settings.javaScriptEnabled = true
         siteWebView.webViewClient = object : WebViewClient(){
+            val progressBar: ProgressBar = view.findViewById(R.id.progressBar)
             override fun shouldOverrideUrlLoading(
                     view: WebView,
                     url: String
@@ -35,8 +47,43 @@ class SiteFragment : Fragment() {
                     view.loadUrl(url)
                     return true
                     }
+
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                progressBar.visibility = View.VISIBLE
+                super.onPageStarted(view, url, favicon)
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?){
+                progressBar.visibility = View.GONE
+                super.onPageFinished(view, url)
+            }
+
+//            override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+//                // Check whether the key event is the Back button and if there's history.
+//                if (keyCode == KeyEvent.KEYCODE_BACK && siteWebView.canGoBack()) {
+//                    siteWebView.goBack()
+//                    return true
+//                }
+//                // If it isn't the Back button or there isn't web page history, bubble up to
+//                // the default system behavior. Probably exit the activity.
+//                return super.onKeyDown(keyCode, event)
+//            }
+
         }
         siteWebView.loadUrl("https://icleborobot.by/product-category/aksessuary/")
+
+        swipeRefreshLayout = view.findViewById(R.id.swipe_fragment_site)
+        swipeRefreshLayout.setOnRefreshListener {
+            Handler(Looper.getMainLooper()).postDelayed({
+                Toast.makeText(requireActivity(), "Обновлено", Toast.LENGTH_LONG).show()
+//                Toast.makeText(this, "Обновлено", Toast.LENGTH_LONG).show()
+                swipeRefreshLayout.isRefreshing = false
+            }, 300)
+
+        }
+
+
+
     }
 
 
@@ -69,5 +116,14 @@ class SiteFragment : Fragment() {
 //        // the default system behavior. Probably exit the activity.
 //        return super.onKeyDown(keyCode, event)
 
+
+
+
+//    override fun onRefresh() {
+//        Handler(Looper.getMainLooper()).postDelayed({
+//            Toast.makeText(this, "Обновлено", Toast.LENGTH_LONG).show()
+//            swipeRefreshLayout.isRefreshing = false
+//        }, 300)
+//    }
 
 }
